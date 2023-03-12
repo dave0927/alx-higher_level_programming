3633cbc97af30e21238635c195276900acbb3dd2
Dawit Tadesse <dawittadesse927@gmail.com>
2023-03-12 09:42:00 +0300

    Checks if a list is parlindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,15 +1,40 @@
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
+#include <stddef.h>
 
 /**
- * main - check the code for Holberton School students.
+ * is_palindrome - Checks if a list is parlindrome
+ * @head: head of linked list
  *
- * Return: Always 0.
+ * Return: 0 if false and 1 if true
  */
-int main(void)
+int is_palindrome(listint_t **head)
 {
-	listint_t *head;
+	listint_t *node;
+	int values[9999], i = 0, c = 0;
 
-	return (0);
+	if ((!*head) || (!head))
+		return (1);
+
+	node = *head;
+
+	if (!node->next)
+		return (1);
+
+	while (node)
+	{
+		values[i] = node->n;
+		node = node->next;
+		i++;
+	}
+	i--;
+	while (i >= 0 && c <= i)
+	{
+		if (values[i] != values[c])
+			return (0);
+		i--;
+		c++;
+	}
+	return (1);
 }
