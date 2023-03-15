d86c049b4351dd6bf2ca7f886f068f03efeff974
Dawit Tadesse <dawittadesse927@gmail.com>
2023-03-15 17:45:48 +0300

    minor fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    if not roman_string:
+        return 0
+    if not isinstance(roman_string, str):
         return 0
 
     roman_to_int_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
