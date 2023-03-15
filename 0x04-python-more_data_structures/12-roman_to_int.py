69f05d74763f51a3ddcd7b4a6ec581b457066e92
Dawit Tadesse <dawittadesse927@gmail.com>
2023-03-15 17:32:00 +0300

    fix: indentation

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,18 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    
+
     roman_to_int_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     previous_value = 0
-    
+
     for char in roman_string[::-1]:
         value = roman_to_int_dict[char]
         if value < previous_value:
             result -= value
         else:
             result += value
-        previous_value = value
-    
+    previous_value = value
+
     return result
 
