5dbee774d1315821f328ae2927f351f618410ade
Dawit Tadesse <dawittadesse927@gmail.com>
2023-06-18 00:05:39 +0300

    Fix: minor

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1],
                          passwd=argv[2],
                          db=argv[3],
-                         host='localhost')
+                         host='localhost',
+                         port=3306)
 
     cur = db.cursor()
 
