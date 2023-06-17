b6e62e1fcc83a5100b9bd27e1225797c5d15237a
Dawit Tadesse <dawittadesse927@gmail.com>
2023-06-17 23:49:54 +0300

    Fix: minor

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,21 +3,24 @@
 get all states module
 '''
 
+
 from sys import argv
 import MySQLdb
 
 
 if __name__ == "__main__":
-    HOST = 'localhost'
-    USER = argv[1]
-    PASSWD = argv[2]
-    DB = argv[3]
-
-    db = MySQLdb.connect(HOST, USER, PASSWD, DB)
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         host='localhost')
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name \
-                LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states\
+                WHERE name LIKE BINARY 'N%'\
+                ORDER BY id ASC")
     states = cur.fetchall()
     for state in states:
         print(state)
+
+    cur.close()
+    db.close()
