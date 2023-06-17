0067f991f7b64b70b6d37bfdf12d72867014a162
Dawit Tadesse <dawittadesse927@gmail.com>
2023-06-17 22:59:19 +0300

    TASK 2: diplay a state from an argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+get all states module
+'''
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    HOST = 'localhost'
+    USER = argv[1]
+    PASSWD = argv[2]
+    DB = argv[3]
+    state_name = argv[4]
+
+    db = MySQLdb.connect(HOST, USER, PASSWD, DB)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id".format(state_name))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
