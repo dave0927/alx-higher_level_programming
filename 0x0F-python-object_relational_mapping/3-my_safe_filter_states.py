a8f5c32ea0d1abe2a389a31ed99e4abe859b6070
Dawit Tadesse <dawittadesse927@gmail.com>
2023-06-18 10:20:37 +0300

    Task 3: list states | SQL injection free

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""""
+Takes in arguments and displays all values
+in the states table of hbtn_0e_0_usa
+(safe from MySQL injections!)
+"""
+
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         host='localhost',
+                         port=3306)
+
+    cur = db.cursor()
+    sql = """SELECT * FROM states 
+            WHERE name = %s 
+            ORDER BY id ASC"""
+
+    cur.execute(sql, (argv[4],))
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+    
+    cur.close()
+    db.close()
