b053803ba736ec1266cf9a1beb99326bd8734d12
Dawit Tadesse <dawittadesse927@gmail.com>
2023-06-18 21:56:25 +0300

    Fix: minor

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-"""Prints the State object with the name passed
-    as argument"""
+""" prints the State object with the name passed
+    as argument from the database.
+    (SQL injection free)
+"""
 
 from sys import argv
-from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from model_state import Base, State
 
-if __name__ == '__main__':
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'
-        .format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True)
-
-    Session = sessionmaker(bind=engine)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
 
     Base.metadata.create_all(engine)
 
+    Session = sessionmaker(bind=engine)
     session = Session()
+
     states = session.query(State).filter(State.name == argv[4]).first()
 
-    if states:
-        print('{}'.format(states.id))
-    else:
+    if states is None:
         print("Not Found")
-
-    session.close()
+    else:
+        print('{}'.format(states.id))
