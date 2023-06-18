0077c3d77d2224ce171fde049a3fba6dad055569
Dawit Tadesse <dawittadesse927@gmail.com>
2023-06-18 15:48:21 +0300

    Task 10: print states with the name passed

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Prints the State object with the name passed
+    as argument"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    Base.metadata.create_all(engine)
+
+    session = Session()
+    states = session.query(State) \
+                    .filter(State.name == argv[4]).one_or_none()
+
+    if states is None:
+        print("Not Found")
+    else:
+        print(states.id)
+
+    session.close()
