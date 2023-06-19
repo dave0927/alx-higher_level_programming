156597571b4bce1043d84f72535e674e46433fd8
Dawit Tadesse <dawittadesse927@gmail.com>
2023-06-19 23:15:55 +0300

    Fix: minor

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,7 +10,7 @@ from sqlalchemy import create_engine
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}' \
                            .format(argv[1], argv[2], argv[3]))
 
     Base.metadata.create_all(engine)
