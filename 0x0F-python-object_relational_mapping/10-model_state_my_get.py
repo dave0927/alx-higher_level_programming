dada17a3969a32cdb381cc113d5f36fd15f92c7e
Dawit Tadesse <dawittadesse927@gmail.com>
2023-06-22 19:30:12 +0300

    fix: pep8

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -9,8 +9,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from model_state import Base, State
 
+
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}' \
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1], argv[2], argv[3]))
 
     Base.metadata.create_all(engine)
