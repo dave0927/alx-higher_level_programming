2d5c9392fdd7941d1c63ada137cc699614e09293
Dawit Tadesse <dawittadesse927@gmail.com>
2023-06-30 23:23:57 +0300

    TASK 6: finds a peak in a list of unsorted int

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,6 +8,7 @@ def find_peak(list_of_integers):
         list_of_integers(int): list of integers to find peak of
     Returns: peak of list_of_integers or None
     """
+
     size = len(list_of_integers)
     mid_e = size
     mid = size // 2
