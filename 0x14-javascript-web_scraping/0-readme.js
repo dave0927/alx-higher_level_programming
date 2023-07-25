47aba7256c151142d54e4c080e61efe7775f8f61
Dawit Tadesse <dawittadesse927@gmail.com>
2023-07-25 21:33:01 +0300

    Task 0: reads & prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 
-fs.readFile(process.argv[2], "utf8", function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    process.stdout.write(data);
-  }
-});
+try {
+  const filename = process.argv[2];
+  const content = fs.readFileSync(filename, 'utf8');
+  console.log(content);
+} catch (err) {
+  if (err.code === 'ENOENT') console.log(err);
+  else if (err.code === 'ERR_INVALID_ARG_TYPE') console.log('filename not found!');
+}
