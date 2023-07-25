628fc1c1f7e79be08d2de433d57046e9d91df2ce
Dawit Tadesse <dawittadesse927@gmail.com>
2023-07-25 21:42:33 +0300

    Task 1: writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 
-try {
-  const filename = process.argv[2];
-  const data = process.argv[3];
-  fs.writeFileSync(filename, data, "utf8");
-} catch (err) {
-  if (err.code === "ERR_INVALID_ARG_TYPE")
-    console.log("0 or not all arguments supply");
-  else if (err.code instanceof Error) console.log(err);
-}
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
