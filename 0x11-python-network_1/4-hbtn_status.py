863bfa590fe88c67162db0a69d8ee905168b96dc
Dawit Tadesse <114774535+dave0927@users.noreply.github.com>
2023-11-06 19:47:17 +0300

    Delete 0x11-python-network_1 directory

--- 0x11-python-network_1/4-hbtn_status.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""initialize"""
-import requests
-
-
-req = requests.get('https://alx-intranet.hbtn.io/status')
-print('Body response:\n\t- type: {}\n\t- content: {}'.
-      format(type(req.text), req.text))
