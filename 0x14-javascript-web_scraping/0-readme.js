8a6134e8367d156202a3f0aa36c72cccb95595b6
Dawit Tadesse <114774535+dave0927@users.noreply.github.com>
2024-07-31 23:11:48 +0300

    fix: refactor

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-try {
-  const filename = process.argv[2];
-  const content = fs.readFileSync(filename, 'utf8');
-  console.log(content);
-} catch (err) {
-  if (err.code === 'ENOENT') console.log(err);
-  else if (err.code === 'ERR_INVALID_ARG_TYPE') console.log('filename not found!');
-}
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
